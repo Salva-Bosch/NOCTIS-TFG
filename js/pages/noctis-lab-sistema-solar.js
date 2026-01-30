832cbde795565ccedb3ee975fefc73b411b52e43
Salva-Bosch <sbosch@campuscamarafp.com>
2026-01-30 21:15:20 +0100

    Inicio de sistema solar otra vez...

---
+++ js/pages/noctis-lab-sistema-solar.js
@@ -0,0 +1,7 @@
+// ============================================
+// NOCTIS LAB - SISTEMA SOLAR
+// Carruseles de planetas rocosos y gaseosos
+// ============================================
+
+// TODO: Implementar carruseles
+console.log('Sistema Solar - JS Cargado');
