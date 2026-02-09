0a0e60f2aacd4616275258d1ccd12becba04dcd9
rodrigoalapont <ralapont@campuscamarafp.com>
2026-02-09 12:52:26 +0100

    Cambios de rutas de ajustes

--- js/guards/sessionGuard.js
+++ js/guards/sessionGuard.js
@@ -2,7 +2,7 @@
 import { auth } from "../core/firebase.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-export function requireSession(redirect = "/pages/public/auth.html") {
+export function requireSession(redirect = "../../public/auth.html") {
   return new Promise((resolve) => {
     onAuthStateChanged(auth, (user) => {
       if (!user) {
